54197c8fa8168fff73c9d670dc88419bf6d7bccc
AdamMc331 <amcneilly331@gmail.com>
2015-09-12 04:31:13 -0400

    More StartGameActivity stuff.

--- app/src/main/java/adammcneilly/capturethetag/MainActivity.java
+++ app/src/main/java/adammcneilly/capturethetag/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
 
     @Override
