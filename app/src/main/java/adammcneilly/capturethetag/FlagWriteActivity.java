96029470f14230596d1d9e8fe0fd4b376d75913d
Tristan Wiley <trwiley@twcny.rr.com>
2015-09-12 04:19:18 -0400

    Merge branch 'master' of C:\Users\Tristan\AndroidStudioProjects\Capture-The-Tag with conflicts.

--- app/src/main/java/adammcneilly/capturethetag/FlagWriteActivity.java
+++ app/src/main/java/adammcneilly/capturethetag/FlagWriteActivity.java
@@ -3,10 +3,42 @@ package adammcneilly.capturethetag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class FlagWriteActivity extends AppCompatActivity {
+
+    boolean mWriteMode = false;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mNfcPendingIntent;
+    EditText flagName;
+
+    String gameName = "MHacks";
+    String teamName = "Tradam";
+
+    String mimeType = "application/adammcneilly.capturethetag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +47,102 @@ public class FlagWriteActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        flagName = (EditText) findViewById(R.id.flag_name);
+
+        flagName.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                mNfcAdapter = NfcAdapter.getDefaultAdapter(FlagWriteActivity.this);
+                mNfcPendingIntent = PendingIntent.getActivity(FlagWriteActivity.this, 0,
+                        new Intent(FlagWriteActivity.this, FlagWriteActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+                enableTagWriteMode();
+
+//                new AlertDialog.Builder(FlagWriteActivity.this).setTitle("Touch tag to write")
+//                        .setOnCancelListener(new DialogInterface.OnCancelListener() {
+//                            @Override
+//                            public void onCancel(DialogInterface dialog) {
+//                                disableTagWriteMode();
+//                            }
+//
+//                        }).create().show();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void afterTextChanged(Editable s){}
+        });
+
+    }
+
+    private void enableTagWriteMode() {
+        mWriteMode = true;
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter[] mWriteTagFilters = new IntentFilter[] { tagDetected };
+        mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mWriteTagFilters, null);
+    }
+
+    private void disableTagWriteMode() {
+        mWriteMode = false;
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Tag writing mode
+        if (mWriteMode && NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefRecord record = NdefRecord.createMime(mimeType, getFlagString().getBytes());
+            NdefMessage message = new NdefMessage(new NdefRecord[] { record });
+            if (writeTag(message, detectedTag)) {
+                Toast.makeText(this, "Success: Wrote placeid to nfc tag", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    /*
+    * Writes an NdefMessage to a NFC tag
+    */
+    public boolean writeTag(NdefMessage message, Tag tag) {
+        int size = message.toByteArray().length;
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag not writable",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                if (ndef.getMaxSize() < size) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag too small",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                ndef.writeNdefMessage(message);
+                return true;
+            } else {
+                NdefFormatable format = NdefFormatable.get(tag);
+                if (format != null) {
+                    try {
+                        format.connect();
+                        format.format(message);
+                        return true;
+                    } catch (IOException e) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getFlagString(){
+        return gameName + "," + teamName + "," + flagName.getText().toString();
     }
 }
--- app/src/main/java/adammcneilly/capturethetag/MainActivity.java
+++ app/src/main/java/adammcneilly/capturethetag/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
     @Override
